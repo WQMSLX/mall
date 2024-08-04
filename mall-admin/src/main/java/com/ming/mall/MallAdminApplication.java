cd02618ca04175c5997479f429b4b45dc552312b
wangqiming <1359632259@qq.com>
2024-08-04 22:36:00 +0800

    mall-admin初始化

---
+++ mall-admin/src/main/java/com/ming/mall/MallAdminApplication.java
@@ -0,0 +1,16 @@
+package com.ming.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author WangQiming
+ */
+@SpringBootApplication
+public class MallAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallAdminApplication.class, args);
+    }
+
+}
